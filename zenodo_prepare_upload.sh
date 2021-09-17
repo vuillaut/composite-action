d12f08918eb20c96b90ae4a463ddb03f1cf4ace8
Thomas Vuillaume <thomas.vuillaume@gmail.com>
2021-09-17 15:54:32 +0200

    Update zenodo_prepare_upload.sh

--- zenodo_prepare_upload.sh
+++ zenodo_prepare_upload.sh
@@ -1,24 +1,25 @@
 OUTPUT_DIR=$1
+REPOSITORY_URL=$2
+
+echo "output dir: $OUTPUT_DIR"
+echo "repository url: $REPOSITORY_URL"
 
 mkdir -p $OUTPUT_DIR
 
 IFS='/'
-read -a strarr <<< $GITHUB_REPOSITORY
+read -a strarr <<< $REPOSITORY_URL
 GITHUB_ORG=${strarr[0]}
-PROJECT_NAME=${strarr[1]}
-REPOSITORY_BASE_URL=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY
-echo $GITHUB_REPOSITORY
-echo $GITHUB_ORG
-echo $PROJECT_NAME
-echo $REPOSITORY_BASE_URL
+OWNER_NAME=${strarr[1]}
+PROJECT_NAME=${strarr[2]}
 
+echo "Deduced project name: $PROJECT_NAME"
 
 LAST_RELEASE=`git tag | tail -1`
 if [ -z "$LAST_RELEASE" ]; then LAST_RELEASE="master"; fi;
 echo "Last release is $LAST_RELEASE"
 
 ARCHIVE="$PROJECT_NAME-$LAST_RELEASE.zip"
-wget -O $ARCHIVE $REPOSITORY_BASE_URL/archive/$LAST_RELEASE.zip
+wget -O $ARCHIVE $REPOSITORY_URL/archive/$LAST_RELEASE.zip
 ls .
 mv $ARCHIVE $OUTPUT_DIR
 
