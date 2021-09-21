04d234b867c26b9961205e5c5df6cf317fd4bbc2
Thomas Vuillaume <thomas.vuillaume@gmail.com>
2021-09-21 13:23:16 +0200

    Update zenodo_prepare_upload.sh

--- zenodo_prepare_upload.sh
+++ zenodo_prepare_upload.sh
@@ -18,7 +18,7 @@ echo "Deduced project name: $PROJECT_NAME"
 if [ -z "$RELEASE" ];
 then
   LAST_RELEASE=`git tag | tail -1`;
-  if [ -z "$LAST_RELEASE" ]; then LAST_RELEASE="main"; fi;
+  # if [ -z "$LAST_RELEASE" ]; then LAST_RELEASE="main"; fi;
   RELEASE=$LAST_RELEASE
 fi;
 echo "Upload based on $RELEASE";
